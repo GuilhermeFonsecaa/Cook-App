402ba0a67232ae4925051855aa78a691f710d735
Guilherme Fonseca <guilhermao1234@hotmail.com>
2024-03-10 04:22:34 -0300

    feat: add preparationsService

---
+++ src/app/services/index.ts
@@ -0,0 +1,13 @@
+import * as recipes from "./recipesService"
+import * as ingredients from "./ingredientsService"
+import * as preparations from "./preparationsService"
+
+export const services = {
+  recipes,
+  ingredients,
+  preparations,
+
+  storage: {
+    imagePath: "https://nysqrsygugzuqsaitqwy.supabase.co/storage/v1/object/public/ingredients",
+  },
+}
