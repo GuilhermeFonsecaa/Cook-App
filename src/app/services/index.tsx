3d269568561ca5726fe33a1c84c6c3d902f9921f
Guilherme Fonseca <guilhermao1234@hotmail.com>
2024-03-10 04:21:34 -0300

    feat: add recipe service

--- src/app/services/index.tsx
+++
@@ -1,8 +0,0 @@
-import * as ingredients from "./ingredientsService"
-
-export const services = {
-    ingredients,
-    storage: {
-        imagePath: "https://nysqrsygugzuqsaitqwy.supabase.co/storage/v1/object/public/ingredients",
-    }
-}
