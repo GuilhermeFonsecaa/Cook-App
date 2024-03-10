269ad8ee99603cfdf961ecc86ca25b5c799c055d
Guilherme Fonseca <guilhermao1234@hotmail.com>
2024-03-10 04:18:09 -0300

    feat: add find by ids and find by recipe id on ingredients service

--- src/app/services/ingredientsService.ts
+++ src/app/services/ingredientsService.ts
@@ -1,8 +1,34 @@
 import { supabase } from "./supabase"
 
-export async function findAll() {
-  const {data} =  await supabase.from("Ingredients").select().order("name").returns<IngredientResponse[]>()
+async function findByIds(ids: string[]) {
+  const { data } = await supabase
+    .from("ingredients")
+    .select()
+    .in("id", ids)
+    .order("name")
+    .returns<IngredientResponse[]>()
 
-    return data ?? []
+  return data ?? []
 }
 
+async function findByRecipeId(id: string) {
+  const { data } = await supabase
+    .from("recipes_ingredients")
+    .select("ingredients (id, name, image)")
+    .eq("recipe_id", id)
+    .returns<{ ingredients: IngredientResponse }[]>()
+
+  return data ? data.map((item) => item.ingredients) : []
+}
+
+async function findAll() {
+  const { data } = await supabase
+    .from("ingredients")
+    .select()
+    .order("name")
+    .returns<IngredientResponse[]>()
+
+  return data ?? []
+}
+
+export { findAll, findByIds, findByRecipeId }
